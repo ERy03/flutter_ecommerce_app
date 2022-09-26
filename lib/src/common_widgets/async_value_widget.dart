a9ad8402cdcd109cea407c64693b17571c2bdc79
Ryota <ryoenoki2013@gmail.com>
2022-09-26 17:32:17 +0900

    created reusable widget for async value

---
+++ lib/src/common_widgets/async_value_widget.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AsyncValueWidget<T> extends StatelessWidget {
+  const AsyncValueWidget({Key? key, required this.data, required this.value})
+      : super(key: key);
+  final AsyncValue<T> value;
+  final Widget Function(T) data;
+
+  @override
+  Widget build(BuildContext context) {
+    return value.when(
+        data: data,
+        error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
+        loading: () => const Center(child: CircularProgressIndicator()));
+  }
+}
