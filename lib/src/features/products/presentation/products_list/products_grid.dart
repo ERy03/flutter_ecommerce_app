bd7580f3f59186df2d64a89832fd4ace31235e03
Ryota <ryoenoki2013@gmail.com>
2022-09-26 17:52:14 +0900

    using async value widget

--- lib/src/features/products/presentation/products_list/products_grid.dart
+++ lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 
+import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
 import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/routing/app_router.dart';
@@ -18,29 +20,29 @@ class ProductsGrid extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final productsListValue = ref.watch(productsListStreamProvider);
-    return productsListValue.when(
-        data: (products) => products.isEmpty
-            ? Center(
-                child: Text(
-                  'No products found'.hardcoded,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              )
-            : ProductsLayoutGrid(
-                itemCount: products.length,
-                itemBuilder: (_, index) {
-                  final product = products[index];
-                  return ProductCard(
-                    product: product,
-                    onPressed: () => context.goNamed(
-                      AppRoute.product.name,
-                      params: {'id': product.id},
-                    ),
-                  );
-                },
+    return AsyncValueWidget<List<Product>>(
+      value: productsListValue,
+      data: (products) => products.isEmpty
+          ? Center(
+              child: Text(
+                'No products found'.hardcoded,
+                style: Theme.of(context).textTheme.headline4,
               ),
-        error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
-        loading: () => const Center(child: CircularProgressIndicator()));
+            )
+          : ProductsLayoutGrid(
+              itemCount: products.length,
+              itemBuilder: (_, index) {
+                final product = products[index];
+                return ProductCard(
+                  product: product,
+                  onPressed: () => context.goNamed(
+                    AppRoute.product.name,
+                    params: {'id': product.id},
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
 
