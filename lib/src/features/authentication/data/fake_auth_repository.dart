8f943b6059c2a8cff17112656db07f2a1b872cd5
Ryota <ryoenoki2013@gmail.com>
2022-09-27 16:25:21 +0900

    fake auth repository

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+
 class FakeAuthRepository {
+  Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: Update
+
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     // TODO: Implement
   }
