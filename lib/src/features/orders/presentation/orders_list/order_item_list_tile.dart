242d0d66e633bfa02dcffd1775222db17964a261
Ryota <ryoenoki2013@gmail.com>
2022-09-27 15:45:12 +0900

    added shimmer package

--- lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
+++ lib/src/features/orders/presentation/orders_list/order_item_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
+import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
@@ -7,6 +8,7 @@ import 'package:ecommerce_app/src/common_widgets/custom_image.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
 import 'package:ecommerce_app/src/features/cart/domain/item.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shimmer/shimmer.dart';
 
 /// Shows an individual order item, including price and quantity.
 class OrderItemListTile extends ConsumerWidget {
@@ -17,8 +19,7 @@ class OrderItemListTile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final productValue = ref.watch(productProvider(item.productId));
 
-    return AsyncValueWidget<Product?>(
-      value: productValue,
+    return productValue.when(
       data: (product) => Padding(
         padding: const EdgeInsets.symmetric(vertical: Sizes.p8),
         child: Row(
@@ -43,6 +44,57 @@ class OrderItemListTile extends ConsumerWidget {
               ),
             ),
           ],
+        ),
+      ),
+      error: (e, st) => Center(child: ErrorMessageWidget(e.toString())),
+      loading: () => Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Shimmer.fromColors(
+            baseColor: Colors.grey.shade300,
+            highlightColor: Colors.grey.shade100,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 48.0,
+                  height: 48.0,
+                  color: Colors.white,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: double.infinity,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2.0),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2.0),
+                      ),
+                      Container(
+                        width: 40.0,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/src/features/products/presentation/products_list/products_grid.dart
+++ lib/src/features/products/presentation/products_list/products_grid.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
-
 import 'package:ecommerce_app/src/common_widgets/async_value_widget.dart';
-import 'package:ecommerce_app/src/common_widgets/error_message_widget.dart';
 import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
 import 'package:ecommerce_app/src/features/products/domain/product.dart';
 import 'package:ecommerce_app/src/features/products/presentation/products_list/product_card.dart';
