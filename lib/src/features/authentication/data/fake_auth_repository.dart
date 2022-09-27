3f97bd9d921e484fd8b81ed89aa5414eced33de4
Ryota <ryoenoki2013@gmail.com>
2022-09-27 16:26:17 +0900

    fake auth repository get current user

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 
 class FakeAuthRepository {
   Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: Update
+  AppUser? get currentUser => null; //TODO: Update
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     // TODO: Implement
