f493737e9a3f96691e7dd1ac1cef062b47daf8f2
Ryota <ryoenoki2013@gmail.com>
2022-09-27 16:23:36 +0900

    created fake auth repository

---
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -0,0 +1,14 @@
+class FakeAuthRepository {
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    // TODO: Implement
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    // TODO: Implement
+  }
+
+  Future<void> signOut() async {
+    // TODO: implement
+  }
+}
